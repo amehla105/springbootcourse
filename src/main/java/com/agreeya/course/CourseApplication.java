013ab7b17f8b1c180e97e09234e393f7ec25a9d7
Anil Kumar <79670700+amehla105@users.noreply.github.com>
2022-02-09 14:35:31 +0530

    course

---
+++ src/main/java/com/agreeya/course/CourseApplication.java
@@ -0,0 +1,20 @@
+package com.agreeya.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CourseApplication  extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(CourseApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
